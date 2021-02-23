368f252f8543e6eef18fe9de7f0d402902013aa8
dankondr <dankondr@icloud.com>
2021-02-23 14:19:24 +0300

    Update

---
+++ cmake-build-debug/CMakeFiles/moonlight-common-c.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.F9sPPF8C56/enet/callbacks.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/callbacks.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/compress.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/compress.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/host.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/host.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/list.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/list.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/packet.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/packet.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/peer.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/peer.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/protocol.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/protocol.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/unix.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/unix.c.o"
+  "/tmp/tmp.F9sPPF8C56/enet/win32.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/enet/win32.c.o"
+  "/tmp/tmp.F9sPPF8C56/reedsolomon/rs.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/reedsolomon/rs.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/AudioStream.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/AudioStream.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/ByteBuffer.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/ByteBuffer.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/Connection.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/Connection.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/ConnectionTester.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/ConnectionTester.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/ControlStream.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/ControlStream.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/FakeCallbacks.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/FakeCallbacks.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/InputStream.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/InputStream.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/LinkedBlockingQueue.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/LinkedBlockingQueue.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/Misc.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/Misc.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/Platform.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/Platform.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/PlatformSockets.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/PlatformSockets.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/PortConfigCoder.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/PortConfigCoder.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/Ports.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/Ports.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/RtpFecQueue.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/RtpFecQueue.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/RtpReorderQueue.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/RtpReorderQueue.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/RtspConnection.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/RtspConnection.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/RtspParser.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/RtspParser.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/SdpGenerator.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/SdpGenerator.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/SimpleStun.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/SimpleStun.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/VideoDepacketizer.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/VideoDepacketizer.c.o"
+  "/tmp/tmp.F9sPPF8C56/src/VideoStream.c" "/tmp/tmp.F9sPPF8C56/cmake-build-debug/CMakeFiles/moonlight-common-c.dir/src/VideoStream.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAS_SOCKLEN_T"
+  "LC_DEBUG"
+  "moonlight_common_c_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../enet/include"
+  "../reedsolomon"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
